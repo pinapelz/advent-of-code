afec594cfe960c859b2449fa471e80082297a40d
Pinapelz <yukais@pinapelz.com>
2023-12-05 12:30:07 -0800

    Python bruteforce day5part2 VERY LONG RUNTIME!

---
+++ 2023/Day5/part2-ver2.py
@@ -0,0 +1,44 @@
+
+def find_mapping(seed, mapping):
+    for rule in mapping:
+        dest_start = rule[0]
+        source_start = rule[1]
+        length = rule[2]
+        source_max = source_start + length
+        if seed >= source_start and seed <= source_max:
+            offset = seed - source_start
+            return dest_start + offset
+    return seed
+
+with open("input.txt", "r") as f:
+    content = [x.strip() for x in f.readlines() if x.strip() != ""]
+    seeds = content[0]
+    seeds = seeds.replace("seeds: ", "")
+    seeds = list(map(int, seeds.split()))
+
+    ranges = []
+    for i in range(0, len(seeds), 2):
+        ranges.append(range(seeds[i], seeds[i]+seeds[i+1]))
+
+    mappings = {}
+    curr_map = ""
+    for line in content[1:]:
+        if "map:" in line:
+            curr_map = line.strip()
+        else:
+            if curr_map in mappings:
+                mappings[curr_map].append(tuple(map(int, line.split())))
+            else:
+                mappings[curr_map] = [tuple(map(int, line.split()))]
+    results = []
+    for r in ranges:
+        for seed in r:
+            soil = find_mapping(seed, mappings["seed-to-soil map:"] )
+            fertilizer = find_mapping(soil, mappings["soil-to-fertilizer map:"])
+            water = find_mapping(fertilizer, mappings["fertilizer-to-water map:"])
+            light = find_mapping(water, mappings["water-to-light map:"])
+            temp = find_mapping(light, mappings["light-to-temperature map:"])
+            humidity = find_mapping(temp, mappings["temperature-to-humidity map:"])
+            location = find_mapping(humidity,mappings["humidity-to-location map:"])
+            results.append(location)
+print(min(results))
